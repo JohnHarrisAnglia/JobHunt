4e6ad3f3cfa8116c88233d0900eeed09d092664b
JohnHarrisDev <johnharrisanglia@gmail.com>
2020-03-12 07:24:00 +0000

    ONLY HAD HALF HOUR ON THIS TODAY -
    Just adding some more of the tutorial i did yetserday.
    
    Also playing with adding google fonts.

--- job-hunt/src/App.vue
+++ job-hunt/src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
     <header>
-      <span>Vue.js PWA</span>
+      <b-navbar toggleable="md" type="light" variant="light">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-navbar-brand to="/">JobHunt</b-navbar-brand>
+        <b-collapse is-nav id="nav-collapse">
+          <b-navbar-nav>
+            <b-nav-item href="#" @click.prevent="login" v-if="!user">Login</b-nav-item>
+            <b-nav-item href="#" @click.prevent="logout" v-else>Logout</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        user: null
+      }
+    },
+    methods: {
+      login () {
+
+      },
+      async logout () {
+
+      }
+    }
+  }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
+
 body {
   margin: 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -37,7 +61,7 @@ header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #f8f9fa;
   color: #ffffff;
 }
 
